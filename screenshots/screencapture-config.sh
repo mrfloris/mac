d1c5ae731f44e8de835d3d11dbe20575ea792dec
Floris Fiedeldij Dop <mrfloris@gmail.com>
2024-11-14 09:20:20 +0100

    - lowered the delay - moved script to be tiny bit more modular, removing repetition

--- screenshots/screencapture-config.sh
+++ screenshots/screencapture-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # @Filename: screencapture-config.sh
-# @Version: 0.1.0, build 002 for macOS 15.1+
+# @Version: 0.1.1, build 003 for macOS 15.1+
 # @Release: November 14th, 2024
 # @Description: Helps me quickly set some screencapture defaults on a new machine
 # @Contact: I am @floris on Twitter, and mrfloris on gmail.
@@ -11,50 +11,55 @@
 
 ### todo
 # - make script dynamic
-# - maybe add a sleep delay
 # - update script to ask upon execution what preferences we want
 # - merge version 1 and 2 together
 # - add startup params so we can just run the script
 # - known issue; if you set a path that doesn't exist, it wont error, it will take the screenshot - but fails and puts it on the default ~/Desktop (so i might want to do a dir check or something)
 
-# Change Screenshot Save Location
-defaults write com.apple.screencapture location -string "~/Desktop/Screeniez"
-sleep 0.2
 
-# Remove Shadows from Window Screenshots
-defaults write com.apple.screencapture disable-shadow -bool false
-sleep 0.2
+# Configuration: Array of macOS screenshot preferences
+preferences=(
+	# Change Screenshot Save Location
+    'defaults write com.apple.screencapture location -string "~/Desktop/Screeniez"'
 
-# Customize the Screenshot File Name Prefix
-defaults write com.apple.screencapture name "screenshot-"
-sleep 0.2
+    # Remove Shadows from Window Screenshots
+    'defaults write com.apple.screencapture disable-shadow -bool false'
 
-# Change the floating Thumbnail time
-# Depending on the system, the expiration with false for thumbnails might still show it, 
-# setting it to 0 will not show it and write to disk, you could always remove this, 
-# reset defaults, and run the script again. But I like it to show and a bit longer.
-defaults write com.apple.screencaptureui thumbnailExpiration -float 12.0
-sleep 0.2
+    # Customize the Screenshot File Name Prefix
+    'defaults write com.apple.screencapture name "screenshot-"'
 
-# Disable Screenshot Thumbnails (for Faster Captures)
-defaults write com.apple.screencapture show-thumbnail -bool true
-sleep 0.2
+    # Change the floating Thumbnail time
+	# Depending on the system, the expiration with false for thumbnails might still show it, 
+	# setting it to 0 will not show it and write to disk, you could always remove this, 
+	# reset defaults, and run the script again. But I like it to show and a bit longer.
+    'defaults write com.apple.screencaptureui thumbnailExpiration -float 12.0'
 
-# Change Screenshot File Format
-defaults write com.apple.screencapture type -string "png"
-sleep 0.2
+    # Disable Screenshot Thumbnails (for Faster Captures)
+    'defaults write com.apple.screencapture show-thumbnail -bool true'
 
-# Add a Custom Capture Delay
-defaults write com.apple.screencapture delay -int 20
-sleep 0.2
+    # Change Screenshot File Format
+    'defaults write com.apple.screencapture type -string "png"'
 
-# Capture the Mouse Cursor in Screenshots
-defaults write com.apple.screencapture include-cursor -bool false
-sleep 0.2
+    # Add a Custom Capture Delay
+    'defaults write com.apple.screencapture delay -int 2'
 
-# Make Backgrounds Transparent for Shadows
-defaults write com.apple.screencapture disable-shadow-background -bool true
-sleep 0.2
+    # Capture the Mouse Cursor in Screenshots
+    'defaults write com.apple.screencapture include-cursor -bool true'
+
+    # Make Backgrounds Transparent for Shadows
+    'defaults write com.apple.screencapture disable-shadow-background -bool true'
+)
+
+# Function to apply each preference with a delay
+apply_preferences() {
+    for cmd in "${preferences[@]}"; do
+        eval "$cmd"
+        sleep 0.1  # Delay to ensure each command is applied
+    done
+}
+
+# Run the function to apply preferences
+apply_preferences
 
 # Restart SystemUIServer
 killall SystemUIServer
