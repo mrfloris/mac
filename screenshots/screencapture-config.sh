dec894fa3f09492991d020071b1cc66d3da82f04
Floris Fiedeldij Dop <mrfloris@gmail.com>
2024-11-14 08:36:02 +0100

    - clean up header, actually have one - set my default eof footer msg

--- screenshots/screencapture-config.sh
+++ screenshots/screencapture-config.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-### header here
+# @Filename: screencapture-config.sh
+# @Version: 0.0.1, build 001 for macOS 15.1+
+# @Release: November 14th, 2024
+# @Description: Helps me quickly set some screencapture defaults on a new machine
+# @Contact: I am @floris on Twitter, and mrfloris on gmail.
+# @Discord: @mrfloris on https://discord.gg/floris
+# @Install: chmod a+x screencapture-config.sh
+# @Syntax: ./screencapture-config.sh (will show help)
+# @URL: Latest source, wiki, & support: https://github.com/mrfloris/mac/tree/main/screenshots
 
-### version
-
-# commands here
 
 # Apply changes
 killall SystemUIServer
 
 echo "Screenshot preferences applied successfully!"
 
-#EOF here
+#EOF Copyright (c) 1977-2024 - Floris Fiedeldij Dop - https://mrfloris.com
